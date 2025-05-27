db9b312fcd35a8b68760549145781d658ef2abed
rohithsiddd <rohith@getswipe.in>
2025-05-27 12:32:50 +0530

    Initial Commit

---
+++ CarbCal/CarbCalApp.swift
@@ -0,0 +1,17 @@
+//
+//  CarbCalApp.swift
+//  CarbCal
+//
+//  Created by rohith siddanathi on 27/05/25.
+//
+
+import SwiftUI
+
+@main
+struct CarbCalApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ CarbCal/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  CarbCal
+//
+//  Created by rohith siddanathi on 27/05/25.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
