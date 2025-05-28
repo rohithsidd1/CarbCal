db6abd7cad877ada7847b29309fe12ddac3f57f6
Rohith Siddanathi <rohithsiddanathi1@gmail.com>
2025-05-29 01:49:09 +0530

    Update APIConfig.swift
    Same! place you Azure OpenAI key to get the perfect results

--- CarbCal/Config/APIConfig.swift
+++ CarbCal/Config/APIConfig.swift
@@ -1,5 +1,5 @@
 import Foundation
 
 enum APIConfig {
-    static let openAIKey = "C4PIM1VYB5GWha0pQNJM7jqkjpTHNp1pTQ7wyz3kd1ZU03Hy3eJLJQQJ99BAACfhMk5XJ3w3AAABACOGkqOR"
+    static let openAIKey = ""
 }
